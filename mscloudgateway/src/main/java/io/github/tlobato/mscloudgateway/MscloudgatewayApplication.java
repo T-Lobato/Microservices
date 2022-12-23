c7964ab5f11833d2b97853ad99da8843d0542972
Thyago Lobato <thyagollobato@gmail.com>
2022-12-23 20:29:23 -0300

    chore: gateway updated with mscard

--- mscloudgateway/src/main/java/io/github/tlobato/mscloudgateway/MscloudgatewayApplication.java
+++ mscloudgateway/src/main/java/io/github/tlobato/mscloudgateway/MscloudgatewayApplication.java
@@ -22,6 +22,7 @@ public class MscloudgatewayApplication {
 		return builder
 				.routes()
 				.route(r -> r.path("/clients/**").uri("lb://msclients"))
+				.route(r -> r.path("/cards/**").uri("lb://mscards"))
 				.build();
 	}
 }
