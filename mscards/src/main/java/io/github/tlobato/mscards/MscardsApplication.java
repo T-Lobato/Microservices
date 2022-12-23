76036ffc6b1726d81a6421c9921669f49d2c79d3
Thyago Lobato <thyagollobato@gmail.com>
2022-12-23 15:46:05 -0300

    feat: Created mscards microservice

---
+++ mscards/src/main/java/io/github/tlobato/mscards/MscardsApplication.java
@@ -0,0 +1,13 @@
+package io.github.tlobato.mscards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscardsApplication.class, args);
+	}
+
+}
---
+++ mscards/src/test/java/io/github/tlobato/mscards/MscardsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tlobato.mscards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
